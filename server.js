38514e679e496aec2db2192f08056ac7701ba880
ganhox28 <nguyenquocanhquan69@gmail.com>
2023-02-13 21:37:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 	res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+	res.send(`Nguyen Quoc Anh Quan`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
