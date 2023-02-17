00845d0f957eae8a1808d75fa959345e60fc7c3a
ganhox28 <nguyenquocanhquan69@gmail.com>
2023-02-17 16:40:28 +0700

    17-02-2023

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -7,6 +7,24 @@ let getHomepage = async (req, res) => {
 	return res.render('index.ejs', {dataUser: rows});
 }
 
+let getDetailUser = async (req, res) => {
+
+	let userId = req.params.userId;
+
+	let [user] = await pool.execute('SELECT * FROM users WHERE ID = ?', [userId]);
+	
+	return res.send(JSON.stringify(user));
+}
+
+let createNewUser = async (req,res) => {
+	let {firstName, lastName, mail, address} = req.body;
+	await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)', 
+		[firstName, lastName, mail, address]);
+	return res.redirect('/');
+}
+
 module.exports = {
-	getHomepage
+	getHomepage,
+	getDetailUser,
+	createNewUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,10 @@ var router = express.Router();
 const initWebRoute = (app) => {
 	router.get('/', homeController.getHomepage);
 
+	router.get('/detail/user/:userId', homeController.getDetailUser);
+
+	router.post('/create-new-user', homeController.createNewUser);
+
 	router.get('/about', (req, res) => {
 		res.send(`Nguyen Quoc Anh Quan`);
 	});
